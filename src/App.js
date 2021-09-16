c9036903dd6616d7d172cef1c438730e40483af3
Ariel Gianatiempo <gianatiempo@gmail.com>
2021-09-16 20:49:56 -0300

    add layout/navigation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import { Layout, Menu } from 'antd';
 import { DollarOutlined, SwapOutlined, LineChartOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import { Switch, Route, useHistory, useLocation } from 'react-router-dom';
+import { Chart, Converter, Cryptocurrency, Optional } from './pages';
 import logo from './assets/logo.jpeg';
 
 const { Content, Sider } = Layout;
 
-const App = () => (
-  <Layout style={{ height: '100vh' }}>
-    <Sider theme='light'>
-      <img src={logo} alt='empirical logo' style={{ height: 44, margin: '8px 6px' }} />
-      <Menu defaultSelectedKeys={['1']} mode='inline'>
-        <Menu.Item key='1' icon={<DollarOutlined />}>
-          Cryptocurrencies
-        </Menu.Item>
-        <Menu.Item key='2' icon={<SwapOutlined />}>
-          Converter
-        </Menu.Item>
-        <Menu.Item key='3' icon={<LineChartOutlined />}>
-          Charts
-        </Menu.Item>
-        <Menu.Item key='4' icon={<QuestionCircleOutlined />}>
-          Optional
-        </Menu.Item>
-      </Menu>
-    </Sider>
+const App = () => {
+  const history = useHistory();
+  const location = useLocation();
+
+  return (
     <Layout style={{ height: '100vh' }}>
-      <Content style={{ margin: '24px', overflow: 'auto' }}>Bill is a cat.</Content>
+      <Sider theme='light'>
+        <img src={logo} alt='empirical logo' style={{ height: 44, margin: '8px 6px' }} />
+        <Menu defaultSelectedKeys={[location.pathname]} mode='inline'>
+          <Menu.Item key='/' icon={<DollarOutlined />} onClick={() => history.push('/')}>
+            Cryptocurrencies
+          </Menu.Item>
+          <Menu.Item key='/converter' icon={<SwapOutlined />} onClick={() => history.push('/converter')}>
+            Converter
+          </Menu.Item>
+          <Menu.Item key='/chart' icon={<LineChartOutlined />} onClick={() => history.push('/chart')}>
+            Charts
+          </Menu.Item>
+          <Menu.Item key='/optional' icon={<QuestionCircleOutlined />} onClick={() => history.push('/optional')}>
+            Optional
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout style={{ height: '100vh' }}>
+        <Content style={{ margin: '24px', overflow: 'auto' }}>
+          <Switch>
+            <Route path='/' exact component={Cryptocurrency} />
+            <Route path='/converter' component={Converter} />
+            <Route path='/chart' component={Chart} />
+            <Route path='/optional' component={Optional} />
+          </Switch>
+        </Content>
+      </Layout>
     </Layout>
-  </Layout>
-);
+  );
+};
 
 export default App;
