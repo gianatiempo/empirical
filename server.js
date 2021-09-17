de7e32392caec55555445450d8f1255928e7fe80
Ariel Gianatiempo <gianatiempo@gmail.com>
2021-09-17 18:54:34 -0300

    add server for crypto page

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const path = require('path');
 
+const rp = require('request-promise');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Put all API endpoints under '/api'
-app.get('/api/test', (req, res) => res.json(['test']));
+app.get('/api/cryptocurrency', async (req, res) => {
+  const start = req.query.start || 1;
+  const limit = req.query.limit || 10;
+
+  const requestOptions = {
+    method: 'GET',
+    uri: 'http://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+    headers: { 'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY },
+    qs: { start, limit, convert: 'USD' },
+    json: true,
+    gzip: true
+  };
+
+  const response = await rp(requestOptions).catch(err => err.error.status);
+  res.json(response);
+});
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/build/index.html')));
