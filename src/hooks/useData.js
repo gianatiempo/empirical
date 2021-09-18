57e50faa905c878ca1f2890c85a596ca99a62825
Ariel Gianatiempo <gianatiempo@gmail.com>
2021-09-18 17:14:29 -0300

    added converter page

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,14 +1,18 @@
 import { useQuery } from 'react-query';
 
 export const useUser = () =>
-  useQuery('user', async () => {
-    const response = await fetch('https://randomuser.me/api/?inc=name,picture');
-    const json = await response.json();
-    if (json.error) {
-      throw new Error('Fetch Random User response was not ok');
-    }
-    return json;
-  });
+  useQuery(
+    'user',
+    async () => {
+      const response = await fetch('https://randomuser.me/api/?inc=name,picture');
+      const json = await response.json();
+      if (json.error) {
+        throw new Error('Fetch Random User response was not ok');
+      }
+      return json;
+    },
+    { refetchOnWindowFocus: false }
+  );
 
 export const useCryptocurrency = ({ current, pageSize }) =>
   useQuery(['cryptocurrency', { current, pageSize }], async ({ queryKey }) => {
@@ -20,3 +24,34 @@ export const useCryptocurrency = ({ current, pageSize }) =>
     }
     return json;
   });
+
+export const useCoin = () =>
+  useQuery(
+    'coin',
+    async () => {
+      const response = await fetch('/api/coin');
+      const json = await response.json();
+      if (json.error) {
+        throw new Error('Fetch Coins response was not ok');
+      }
+      return json;
+    },
+    { refetchOnWindowFocus: false }
+  );
+
+export const useConversion = ({ origin, destination, amount }) =>
+  useQuery(
+    ['convert', { origin, destination, amount }],
+    async ({ queryKey }) => {
+      const response = await fetch(`/api/convert?origin=${origin}&destination=${destination}&amount=${amount}`);
+      const json = await response.json();
+      if (json.error) {
+        throw new Error('Fetch Coins response was not ok');
+      }
+      return json;
+    },
+    {
+      refetchOnWindowFocus: false,
+      enabled: false // turned off by default, manual refetch is needed
+    }
+  );
--- src/pages/Converter/Converter.js
+++ src/pages/Converter/Converter.js
@@ -1,27 +1,21 @@
 import { useState } from 'react';
-import { Card, Col, Row, TreeSelect, InputNumber, Space, Button, Typography } from 'antd';
+import { Card, Col, Row, Select, InputNumber, Space, Button, Typography } from 'antd';
 import { SwapOutlined } from '@ant-design/icons';
-import { useCryptocurrency } from '../../hooks/useData';
+import { useCoin, useConversion } from '../../hooks/useData';
 import { Error, Spinner } from '../../components';
+
+const { Option } = Select;
 
 const cardStyle = { display: 'flex', flexDirection: 'column', marginBottom: 8 };
 
-const treeData = [
-  {
-    title: 'Node1',
-    value: '0-0',
-    selectable: false,
-    children: [
-      { title: 'Child Node1', value: '0-0-1' },
-      { title: 'Child Node2', value: '0-0-2' }
-    ]
-  },
-  { title: 'Node2', value: '0-1', selectable: false }
-];
-
 const Converter = () => {
-  const [conversion, setConversion] = useState({ current: 1, pageSize: 10, value: '0-0-1' });
-  const { isLoading, isError, data, error } = useCryptocurrency(conversion);
+  const [conversion, setConversion] = useState({ origin: null, destination: null, amount: 0 });
+  const { isLoading, isError, data: coins, error } = useCoin();
+  const { data: converted, refetch } = useConversion(conversion);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   if (isError) {
     return <Error message={error.message} />;
@@ -30,49 +24,68 @@ const Converter = () => {
   return (
     <>
       <Card bordered={false} bodyStyle={cardStyle}>
-        {isLoading ? <Spinner /> : null}
-        {data ? (
-          <Space direction='vertical'>
-            <Row>
-              <Col span={10}>
-                <InputNumber style={{ width: '100%' }} min='0' step='0.00000001' stringMode />
-              </Col>
-            </Row>
-            <Row>
-              <Col span={10}>
-                <TreeSelect
-                  style={{ width: '100%' }}
-                  value={conversion.value}
-                  dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-                  treeData={treeData}
-                  placeholder='Please select'
-                  treeDefaultExpandAll
-                  onChange={value => setConversion({ ...conversion, value })}
-                />
-              </Col>
-              <Col span={1} offset={2}>
-                <SwapOutlined />
-              </Col>
-              <Col span={10} offset={1}>
-                <TreeSelect
-                  style={{ width: '100%' }}
-                  value={conversion.value}
-                  dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-                  treeData={treeData}
-                  placeholder='Please select'
-                  treeDefaultExpandAll
-                  onChange={value => setConversion({ ...conversion, value })}
-                />
-              </Col>
-            </Row>
-          </Space>
-        ) : null}
+        <Space direction='vertical'>
+          <Row>
+            <Col span={10}>
+              <InputNumber
+                style={{ width: '100%' }}
+                min='0'
+                step='0.00000001'
+                stringMode
+                onChange={value => setConversion({ ...conversion, amount: value })}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col span={10}>
+              <Select
+                style={{ width: '100%' }}
+                value={conversion.origin}
+                placeholder='Origin COIN'
+                onChange={value => setConversion({ ...conversion, origin: value })}>
+                {coins.data.map(coin => (
+                  <Option key={coin.symbol}>
+                    {coin.name} - {coin.symbol}
+                  </Option>
+                ))}
+              </Select>
+            </Col>
+            <Col span={1} offset={2}>
+              <SwapOutlined />
+            </Col>
+            <Col span={10} offset={1}>
+              <Select
+                style={{ width: '100%' }}
+                value={conversion.destination}
+                placeholder='Destination COIN'
+                onChange={value => setConversion({ ...conversion, destination: value })}>
+                {coins.data.map(coin => (
+                  <Option key={coin.symbol}>
+                    {coin.name} - {coin.symbol}
+                  </Option>
+                ))}
+              </Select>
+            </Col>
+          </Row>
+        </Space>
       </Card>
       <Row>
         <Col span={6} offset={9}>
           <Card bordered={false} bodyStyle={cardStyle}>
-            <Typography.Title level={3}>1234 BTC</Typography.Title>
-            <Button>Convert</Button>
+            {converted ? (
+              <Typography.Title level={3}>
+                {converted.data.quote[conversion.destination].price.toFixed(8)} {conversion.destination}
+              </Typography.Title>
+            ) : (
+              <Typography.Title level={3}>
+                {conversion.amount} {conversion.origin}
+              </Typography.Title>
+            )}
+            <Button
+              onClick={refetch}
+              disabled={conversion.origin === null || conversion.destination === null || conversion.amount <= 0}>
+              Convert
+            </Button>
           </Card>
         </Col>
       </Row>
